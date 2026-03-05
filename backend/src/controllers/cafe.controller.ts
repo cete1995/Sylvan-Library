0e0c380e004209d7e846ae10c73298447125d04a
cete995 <christian03@ciputra.ac.id>
2026-03-05 12:10:58 +0700

    fix: use named import for asyncHandler in cafe.controller.ts

--- backend/src/controllers/cafe.controller.ts
+++ backend/src/controllers/cafe.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import asyncHandler from '../middleware/asyncHandler';
+import { asyncHandler } from '../middleware/asyncHandler';
 import CafeSettings from '../models/CafeSettings.model';
 
 /**
